b12156587673761746c7e85a0180f43f7be9a987
Emily Caroline <emilyy.caroline.silva@gmail.com>
2023-01-23 21:57:19 -0300

    ChessExeption and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,9 +23,14 @@ public class ChessMatch {
         return mat;
     }
 
+    //esse metodo recebe as cordenadas do xadrez
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column,row).toPosition());
+    }
+
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BlACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+        placeNewPiece('b', 6,new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8,new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1,new Rook(board, Color.WHITE));
     }
 }
