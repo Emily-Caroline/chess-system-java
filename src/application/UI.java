0b5b2ddcb1f9a6cb7578de201d560f46cdca7b3a
Emily Caroline <emilyy.caroline.silva@gmail.com>
2023-01-24 21:07:31 -0300

    Handling exceptions and screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -31,6 +31,10 @@ public class UI {
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
     public static ChessPosition readChessPosition(Scanner sc){
         try {
             String s = sc.nextLine();
@@ -39,7 +43,7 @@ public class UI {
             return new ChessPosition(column, row);
         }
         catch (RuntimeException e){
-            throw new InputMismatchException("Erro lendo posião do xadre: Valores validos sao de 01 a h8 ");
+            throw new InputMismatchException("Erro lendo posição do xadre: Valores validos sao de 01 a h8 ");
         }
     }
 
