4fff30b37af6a6b8aca6cebb884d152ca828b8c2
Emily Caroline <emilyy.caroline.silva@gmail.com>
2023-01-24 21:54:26 -0300

    Implementing possible moves of Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,6 +28,7 @@ public class ChessMatch {
         Position source = soursePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
     }
@@ -44,6 +45,12 @@ public class ChessMatch {
         }
         if (!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessExeption("Nao existe movimentos possivies para a peça escolhida ");
+        }
+    }
+
+    private void validateTargetPosition(Position source, Position target){
+        if (!board.piece(source).possibleMove(target)){
+            throw new ChessExeption("A peça escolhida nao pode ser movida para posição de destino");
         }
     }
 
